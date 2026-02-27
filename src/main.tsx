ea1cd70e7c01ce870288e585472b01e454f9e8b8
DevRGS <rafael.g.santos2120@gmail.com>
2026-02-27 15:42:30 -0300

    atualizado main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { SessionContextProvider } from "./contexts/SessionContext.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <SessionContextProvider>
       <App />
     </SessionContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
